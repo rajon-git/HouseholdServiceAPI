9c8e0555e21891f850d468003078f3ae25a2f6a4
rajon-git <rajon.zhsust15@gmail.com>
2024-09-28 10:45:38 +0600

    logout post method create

--- accounts/views.py
+++ accounts/views.py
@@ -73,7 +73,7 @@ class UserLoginApiView(APIView):
         return Response(serializer.errors)
     
 class UserLogoutApiView(APIView):
-    def get(self, request):
+    def post(self, request):
         request.user.auth_token.delete()
         logout(request)
         return redirect('login')
