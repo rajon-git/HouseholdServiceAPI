c5f4b956bf9779f4bf4fd699727e3ec75d763889
rajon-git <rajon.zhsust15@gmail.com>
2024-11-24 15:15:38 +0600

    image path updated

--- HouseholdService/settings.py
+++ HouseholdService/settings.py
@@ -41,17 +41,16 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
-
-# CORS_ALLOW_HEADERS = list(default_headers) + [
-#     'session_key',
-# ]
+CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    'https://householdfront.onrender.com'
+    'https://householdfront.onrender.com',
+    'https://householdserviceapi.onrender.com', 
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://householdserviceapi.onrender.com', 
-    'https://householdfront.onrender.com'
+    'https://householdfront.onrender.com',
+    'https://householdserviceapi.onrender.com'
+    
 ] 
 APPEND_SLASH = False
 
@@ -110,17 +109,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
-CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -145,16 +140,10 @@ REST_FRAMEWORK = {
 }
 
 
-CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
-
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 SESSION_COOKIE_AGE = 3600
 SESSION_SAVE_EVERY_REQUEST = False
-SESSION_COOKIE_SECURE = True  # Use True for cross-origin session cookies
+SESSION_COOKIE_SECURE = True  
 CSRF_COOKIE_SECURE = True
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False
-SESSION_COOKIE_SAMESITE = 'None'  # None for cross-origin requests
+SESSION_COOKIE_SAMESITE = 'None'  
