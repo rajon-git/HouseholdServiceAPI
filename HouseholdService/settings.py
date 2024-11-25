b1e3bbe3884e150d6f9d8aa3683a931c29a13775
rajon-git <rajon.zhsust15@gmail.com>
2024-11-25 15:45:40 +0600

    setting changed

--- HouseholdService/settings.py
+++ HouseholdService/settings.py
@@ -1,182 +1,16 @@
-# from pathlib import Path
-# from decouple import config
-# from corsheaders.defaults import default_headers
-# import os
-
-# BASE_DIR = Path(__file__).resolve().parent.parent
-
-# SECRET_KEY = config('SECRET_KEY')
-
-# DEBUG = True
-
-# ALLOWED_HOSTS = ["*"]
-
-# INSTALLED_APPS = [
-#     'django.contrib.admin',
-#     'django.contrib.auth',
-#     'django.contrib.contenttypes',
-#     'django.contrib.sessions',
-#     'django.contrib.messages',
-#     'django.contrib.staticfiles',
-#     'rest_framework',
-#     'rest_framework.authtoken',
-#     'django_filters',
-#     'corsheaders',
-#     'authapp',
-#     'services',
-#     'cart',
-#     'order',
-    
-# ]
-
-# MIDDLEWARE = [
-#     'corsheaders.middleware.CorsMiddleware', 
-#     'django.middleware.security.SecurityMiddleware',
-#     'django.contrib.sessions.middleware.SessionMiddleware',
-#     'django.middleware.common.CommonMiddleware',
-#     'django.middleware.csrf.CsrfViewMiddleware',
-#     'django.contrib.auth.middleware.AuthenticationMiddleware',
-#     'django.contrib.messages.middleware.MessageMiddleware',
-#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-# ]
-
-# CORS_ALLOW_ALL_ORIGINS = True
-
-# # CORS_ALLOW_HEADERS = list(default_headers) + [
-# #     'session_key',
-# # ]
-
-# CORS_ALLOWED_ORIGINS = [
-#     'https://householdfront.onrender.com'
-# ]
-# CSRF_TRUSTED_ORIGINS = [
-#     'https://householdserviceapi.onrender.com', 
-#     'https://householdfront.onrender.com'
-# ] 
-# APPEND_SLASH = False
-
-# ROOT_URLCONF = 'HouseholdService.urls'
-
-# STATIC_URL = 'static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend/build/static')]
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend/build')
-
-# TEMPLATES = [
-#     {
-#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#         'DIRS': [REACT_APP_DIR],
-#         'APP_DIRS': True,
-#         'OPTIONS': {
-#             'context_processors': [
-#                 'django.template.context_processors.debug',
-#                 'django.template.context_processors.request',
-#                 'django.contrib.auth.context_processors.auth',
-#                 'django.contrib.messages.context_processors.messages',
-#             ],
-#         },
-#     },
-# ]
-
-# WSGI_APPLICATION = 'HouseholdService.wsgi.application'
-
-
-# # Database
-# # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-# # Password validation
-# # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
-# AUTH_PASSWORD_VALIDATORS = [
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-#     },
-# ]
-
-
-# # Internationalization
-# # https://docs.djangoproject.com/en/5.1/topics/i18n/
-
-# LANGUAGE_CODE = 'en-us'
-
-# TIME_ZONE = 'UTC'
-
-# USE_I18N = True
-
-# USE_TZ = True
-# CSRF_TRUSTED_ORIGINS = [
-#     'https://householdserviceapi.onrender.com',
-#     'https://householdfront.onrender.com',  
-# ]
-
-
-
-# # Default primary key field type
-# # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
-# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = config('EMAIL_HOST')
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-
-# DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER')
-
-# REST_FRAMEWORK = {
-#     'DEFAULT_AUTHENTICATION_CLASSES': [
-#         'rest_framework.authentication.SessionAuthentication',
-#         'rest_framework.authentication.BasicAuthentication',
-#         'rest_framework.authentication.TokenAuthentication',  
-#     ],
-# }
-
-
-# CORS_ALLOW_CREDENTIALS = True
-
-# SESSION_ENGINE = 'django.contrib.sessions.backends.db'
-# SESSION_COOKIE_AGE = 3600
-# SESSION_SAVE_EVERY_REQUEST = False
-# SESSION_COOKIE_SECURE = True  # Use True for cross-origin session cookies
-# CSRF_COOKIE_SECURE = True
-# SESSION_EXPIRE_AT_BROWSER_CLOSE = False
-# SESSION_COOKIE_SAMESITE = 'None'  # None for cross-origin requests
-
-
 from pathlib import Path
 from decouple import config
 from corsheaders.defaults import default_headers
 import os
 
-# Base directory of the project
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Security settings
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = True  # Change to False in production!
-ALLOWED_HOSTS = ["*"]  # Specify exact domains in production for security
 
-# Installed apps
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -192,11 +26,11 @@ INSTALLED_APPS = [
     'services',
     'cart',
     'order',
+    
 ]
 
-# Middleware configuration
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',  # Ensure CORS headers are applied first
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -206,26 +40,29 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# CORS settings
-CORS_ALLOW_ALL_ORIGINS = False  # Allow all origins in development only
-CORS_ALLOWED_ORIGINS = [
-    'https://householdfront.onrender.com',
-]
-CORS_ALLOW_CREDENTIALS = True  # Allow credentials like cookies
-CSRF_TRUSTED_ORIGINS = [
-    'https://householdserviceapi.onrender.com',
-    'https://householdfront.onrender.com',
-]
-APPEND_SLASH = False  # Disable automatic appending of slashes to URLs
+CORS_ALLOW_ALL_ORIGINS = True
 
-# Static and media file settings
+# CORS_ALLOW_HEADERS = list(default_headers) + [
+#     'session_key',
+# ]
+
+CORS_ALLOWED_ORIGINS = [
+    'https://householdfront.onrender.com'
+]
+CSRF_TRUSTED_ORIGINS = [
+    'https://householdserviceapi.onrender.com', 
+    'https://householdfront.onrender.com'
+] 
+APPEND_SLASH = False
+
+ROOT_URLCONF = 'HouseholdService.urls'
+
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend/build/static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend/build')
 
-# Template settings
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -242,10 +79,12 @@ TEMPLATES = [
     },
 ]
 
-# WSGI application
 WSGI_APPLICATION = 'HouseholdService.wsgi.application'
 
-# Database configuration
+
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -253,46 +92,72 @@ DATABASES = {
     }
 }
 
+
 # Password validation
+# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
+
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
 ]
 
+
 # Internationalization
+# https://docs.djangoproject.com/en/5.1/topics/i18n/
+
 LANGUAGE_CODE = 'en-us'
+
 TIME_ZONE = 'UTC'
+
 USE_I18N = True
+
 USE_TZ = True
+CSRF_TRUSTED_ORIGINS = [
+    'https://householdserviceapi.onrender.com',
+    'https://householdfront.onrender.com',  
+]
+
+
 
 # Default primary key field type
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
 DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER')
 
-# REST Framework settings
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.TokenAuthentication',  
     ],
 }
 
-# Session settings
+
+CORS_ALLOW_CREDENTIALS = True
+
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
-SESSION_COOKIE_AGE = 3600  # 1 hour
+SESSION_COOKIE_AGE = 3600
 SESSION_SAVE_EVERY_REQUEST = False
-SESSION_COOKIE_SECURE = True  # True for HTTPS
+SESSION_COOKIE_SECURE = True  # Use True for cross-origin session cookies
 CSRF_COOKIE_SECURE = True
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False
-SESSION_COOKIE_SAMESITE = 'None'  # Required for cross-origin cookies
+SESSION_COOKIE_SAMESITE = 'None'  # None for cross-origin requests
