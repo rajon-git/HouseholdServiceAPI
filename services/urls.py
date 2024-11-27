3b2625db0d96a9b7eea86613e7216edcee8eb5d0
rajon-git <rajon.zhsust15@gmail.com>
2024-11-27 10:18:28 +0600

    url updated

--- services/urls.py
+++ services/urls.py
@@ -13,4 +13,4 @@ urlpatterns = [
     path('reviews/<int:pk>/', ReviewListByServiceView.as_view(), name='review-detail'),
     path('discounted/', DiscountedServiceList.as_view(), name='discounted-services'),
     path('related-products/<int:service_id>/', RelatedServiceList.as_view(), name='related-services'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
