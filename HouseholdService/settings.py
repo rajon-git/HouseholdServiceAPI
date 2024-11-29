7cf8df4690d44ecc5ab9a513363b3d4229a3bfd4
rajon-git <rajon.zhsust15@gmail.com>
2024-11-30 00:08:58 +0600

    also added localhost

--- HouseholdService/settings.py
+++ HouseholdService/settings.py
@@ -41,12 +41,14 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'https://householdfront.onrender.com'
+    'https://householdfront.onrender.com',
+    'http://localhost:3000'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://householdserviceapi.onrender.com/', 
-    'https://householdfront.onrender.com'
+    'https://householdfront.onrender.com',
+    'http://localhost:3000'
 ] 
 APPEND_SLASH = False
 
